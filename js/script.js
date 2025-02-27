0839e9810e03080d33217ee6048a30d971764494
lizzie-abuladze <liziabuladze54@gmail.com>
2025-02-27 13:20:58 +0400

    js file

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// 1
+function sumOfNum(...numbers) {
+    let sum = 0;
+    for (let item of numbers){
+        sum +=  item;
+    }
+    return sum;
+}
+let sumResult = sumOfNum(10, 50, 6, 7, 8, 11, 6, 3, 9);
+console.log(sumResult)
+// 3
+function findMaxNumber(numbers) {
+    return Math.max(...numbers);
+  }
+  let array = findMaxNumber([1, 3, 7, 2, 9, 5]);
+  console.log(array)
+//4
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+      return "this number is even";
+    } else {
+      return "this number is odd";
+    }
+  }
+  let evenOrOdd = checkEvenOdd(7)
+  console.log(evenOrOdd); 
+  
+  
